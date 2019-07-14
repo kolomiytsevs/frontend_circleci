62004c7c326e0dcff5d96dbb895ac994efd0f456
Sasha Kolomiytsev <kolomiytsevs@googlemail.com>
2019-07-14 14:11:52 +0100

    parse json

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function reqListener () {
     const data = this.responseText
-  }
+    const parsedData = JSON.parse(data)
+    console.log(parsedData)
+}
   
   const oReq = new XMLHttpRequest()
   oReq.addEventListener("load", reqListener)
   oReq.open("GET", "https://codersuk-test.s3-eu-west-1.amazonaws.com/data.json")
   oReq.send()
 
-  const parsedData = JSON.parse(data)
-  console.log(data)
+  
